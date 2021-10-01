0b833c04d36abe4c254b3a442154e239be1e13ba
pupil <pupil@ivkhk.ee>
2021-10-01 11:08:48 +0300

    First

--- src/myclasses/App.java
+++ src/myclasses/App.java
@@ -17,6 +17,7 @@ public class App {
         //System.out.println("Hello");
         Book book1 = new Book();
         book1.setCaption("Voina i mir");
+        
         Author author1 = new Author();
         author1.setName("Lev");
         author1.setLastname("Tolstoi");
@@ -25,5 +26,21 @@ public class App {
         author1.setMonth(9);
         Author[] authors = new Author[1];
         authors[0]=author1;
+        book1.setAuthor(authors);
+        book1.setPublishedYear(2005);
+        
+        Book book2 = new Book();
+        book2.setCaption("Otsi i deti");
+        
+        Author author2 = new Author();
+        author2.setName("Ivan");
+        author2.setLastname("Turgenev");
+        author2.setYear(1818);
+        author2.setDay(28);
+        author2.setMonth(10);
+        Author[] authors2 = new Author[1];
+        authors2[0]=author2;
+        book2.setAuthor(authors);
+        book2.setPublishedYear(2007);
     }
 }
